fa3045f27f7578c236ccf78928aa8a7a5c01a9af
XENIA <lommy101@gmail.com>
2020-01-14 04:57:27 +0900

    Add files via upload

---
+++ final/sample.py
@@ -0,0 +1,14 @@
+# uncompyle6 version 3.6.2
+# Python bytecode 3.6 (3379)
+# Decompiled from: Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)]
+# Embedded file name: sample.py
+# Compiled at: 1995-09-28 01:18:56
+# Size of source mod 2**32: 3062 bytes
+
+
+def Hello():
+    print('Hello World !')
+
+
+if __name__ == '__main__':
+    Hello()
